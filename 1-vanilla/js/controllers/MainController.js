b502d52d6d0e64bba49d63acbacb0ee4132f129d
sukio-1024 <sukio1024@naver.com>
2018-07-25 04:49:41 +0900

    HistoryView
    1. make HistoryView
    2. fetchSearchKeyword, fetchSearchHistory
    3. onClickHistory - search

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,11 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 import KeywordView from '../views/KeyWordView.js'
+import HistoryView from '../views/HistoryView.js'
 
 import SearchModel from '../models/SearchModel.js'
 import KeywordModel from '../models/KeywordModel.js'
+import HistoryModel from '../models/HistoryModel.js'
 
 const tag = '[MainController]'
 
@@ -19,9 +21,13 @@ export default {
 
     KeywordView.setup(document.querySelector('#search-keyword'))
       .on('@click', e => this.onClickKeyword(e.detail.keyword))
+
+    HistoryView.setup(document.querySelector('#search-history'))
+      .on('@click', e => this.onClickHistory(e.detail.keyword))
+
     ResultView.setup(document.querySelector('#search-result'))
 
-    this.selectedTab = '추천 검색어'
+    this.selectedTab = '최근 검색어'
     this.renderView()
   },
 
@@ -30,17 +36,28 @@ export default {
     TabView.setActiveTab(this.selectedTab)
 
     if (this.selectedTab === '추천 검색어'){
-      KeywordModel.list().then(data => {
-        KeywordView.render(data)
-      })
+      this.fetchSearchKeyword()
     } else{
-
+        this.fetchSearchHistory()
     }
 
     ResultView.hide()
   },
 
+  fetchSearchKeyword(){
+    KeywordModel.list().then(data => {
+      KeywordView.render(data)
+    })
+  },
+
+  fetchSearchHistory(){
+    HistoryModel.list().then(data => {
+      HistoryView.render(data)
+    })
+  },
+
   search(query) {
+    FormView.setValue(query)
     SearchModel.list(query).then(data => {
       this.onSearchResult(data)
     })
@@ -53,7 +70,7 @@ export default {
 
   onResetForm() {
     console.log(tag, 'onResetForm()')
-    ResultView.hide()
+    this.renderView()
   },
 
   onSearchResult(data) {
@@ -67,6 +84,10 @@ export default {
   },
 
   onClickKeyword(keyword){
+    this.search(keyword)
+  },
+
+  onClickHistory(keyword){
     this.search(keyword)
   }
 
