c7607dd92e69d8b23d71ce71899df064c8e16d1b
sukio-1024 <sukio1024@naver.com>
2018-07-25 01:25:04 +0900

    TabView-(bindClick,onClick)
    1. bindClick - find <li> click

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,47 +1,55 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
+
 import SearchModel from '../models/SearchModel.js'
+
 const tag = '[MainController]'
 
 export default {
-    init() {
-        FormView.setup(document.querySelector('form'))
-            .on('@submit', e => this.onSubmit(e.detail.input))
-            .on('@reset', e => this.onResetForm())
+  init() {
+    FormView.setup(document.querySelector('form'))
+      .on('@submit', e => this.onSubmit(e.detail.input))
+      .on('@reset', e => this.onResetForm())
 
-        TabView.setup(document.querySelector('#tabs'))
+    TabView.setup(document.querySelector('#tabs'))
+      .on('@change', e => this.onChangeTab(e.detail.tabName))
 
-        ResultView.setup(document.querySelector('#search-result'))
+    ResultView.setup(document.querySelector('#search-result'))
 
-        this.selectedTab = '추천 검색어'
-        this.renderView()
-    },
+    this.selectedTab = '추천 검색어'
+    this.renderView()
+  },
 
-    renderView(){
-        console.log(tag,'renderView()')
-        TabView.setActiveTab(this.selectedTab)
-        ResultView.hide()
-    },
-    search(query){
-        console.log(tag, 'search()' , query)
-        SearchModel.list(query).then(data => {
-            this.onSearchResult(data)
-        })
-        
-    },
+  renderView() {
+    console.log(tag, 'rednerView()')
+    TabView.setActiveTab(this.selectedTab)
 
-    onSubmit(input){
-        console.log(tag, 'onSubmit', input)
-        this.search(input)
-    },
+    ResultView.hide()
+  },
 
-    onResetForm(){
-        console.log(tag, 'onResetForm()')
-        ResultView.hide()
-    },
+  search(query) {
+    SearchModel.list(query).then(data => {
+      this.onSearchResult(data)
+    })
+  },
 
-    onSearchResult(data){
-        ResultView.render(data)
-    }
+  onSubmit(input) {
+    console.log(tag, 'onSubmit()', input)
+    this.search(input)
+  },
+
+  onResetForm() {
+    console.log(tag, 'onResetForm()')
+    ResultView.hide()
+  },
+
+  onSearchResult(data) {
+    ResultView.render(data)
+  },
+
+  onChangeTab(tabName) {
+    debugger
+  },
+
 }
