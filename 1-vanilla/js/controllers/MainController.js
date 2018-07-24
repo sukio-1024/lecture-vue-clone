6cb77578c6d8203791c9bea870599ff487c7b28e
sukio-1024 <sukio1024@naver.com>
2018-07-24 21:13:30 +0900

    FormView5
    1. ResultView
    2. search

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,5 +1,5 @@
 import FormView from '../views/FormView.js'
-
+import ResultView from '../views/ResultView.js'
 const tag = '[MainController]'
 
 export default {
@@ -7,13 +7,26 @@ export default {
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+    search(query){
+        console.log(tag, 'search()' , query)
+        //search api
+        this.onSearchResult([])
     },
 
     onSubmit(input){
         console.log(tag, 'onSubmit', input)
+        this.search(input)
     },
 
     onResetForm(){
         console.log(tag, 'onResetForm()')
+    },
+
+    onSearchResult(data){
+        ResultView.render(data)
     }
 }
